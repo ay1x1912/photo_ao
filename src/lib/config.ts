f49579a35e437d619446f6181eb16744054ef897
Ayaan <ayaanmemon1912@gmail.com>
2025-05-10 13:29:17 +0530

    backenurl updated

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const  BACKEND_URL="http://localhost:3000"
+export const  BACKEND_URL="https://photo-ao-nzmf-git-main-ayaan-memons-projects.vercel.app"
 export const   ZIP_URL="https://pub-eb32829f705f4084b8b957e333d06772.r2.dev"
